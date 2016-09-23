d6ff43b073ab2aa7ae7cc85d1a4af18a441504f7
GAMELAB\pas4981 <paul.me.smith@gmail.com>
2016-09-23 16:41:33 -0500

    add player select screens

---
+++ Assets/AIPLayerPanel.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIPLayerPanel : MonoBehaviour {
+    public GameObject[] buttons;
+     
+	// Use this for initialization
+	void Start () {
+        switch (GameManager.NumHumanPLayers)
+        {
+            case 1:
+                break;
+            case 2:
+                buttons[3].SetActive(false);
+                break;
+            case 3:
+                buttons[3].SetActive(false);
+                buttons[2].SetActive(false);
+                break;
+            case 4:
+                buttons[3].SetActive(false);
+                buttons[2].SetActive(false);
+                buttons[1].SetActive(false);
+                break;
+            case 5:
+                buttons[3].SetActive(false);
+                buttons[2].SetActive(false);
+                buttons[1].SetActive(false);
+                buttons[0].SetActive(false);
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetAIPlayer(int i)
+    {
+        GameManager.NumAIPLayers = i;
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main scene");
+        
+    }
+}
---
+++ Assets/HumanPlayerPannel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanPlayerPannel : MonoBehaviour {
+    public GameObject BotPlayerPannel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void SetHumanPlayer(int i)
+    {
+        GameManager.NumHumanPLayers = i;
+        BotPlayerPannel.SetActive(true);
+        gameObject.SetActive(false);
+    } 
+}
