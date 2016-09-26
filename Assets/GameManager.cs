ac7bb42e8634b72e553d561e58e45523f39b5017
GAMELAB\pas4981 <paul.me.smith@gmail.com>
2016-09-26 15:23:04 -0500

    Added player labels that populate on main scene start

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-    public static int NumHumanPLayers;
-    public static int NumAIPLayers;
+    public static int NumHumanPLayers = 2;
+    public static int NumAIPLayers = 2;
     // Use this for initialization
     void Start () {
 	
---
+++ Assets/PlayerList.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerList : MonoBehaviour {
+
+    public GameObject[] playerEntries;
+
+	// Assumes GameManager.NumHumanPlayers and NumAIPlayers is already set
+	void Start () {
+        for (int i = 0; i < playerEntries.Length; i++)
+        {
+            if (i < GameManager.NumHumanPLayers)
+                SetupLabel(playerEntries[i], i, false);
+            else if (i < GameManager.NumHumanPLayers + GameManager.NumAIPLayers)
+                SetupLabel(playerEntries[i], i, true);
+            else
+                playerEntries[i].SetActive(false);
+        }
+    }
+
+    void SetupLabel(GameObject label, int playerNumber, bool isBot)
+    {
+        label.name = label.name = (isBot) ? "Bot player" : "Human player";
+        label.GetComponentInChildren<Text>().text = ((isBot) ? "Bot player " : "Human player ") + playerNumber.ToString();
+        label.GetComponent<Image>().color = (playerNumber == 0) ? Color.green : (playerNumber == 1) ? Color.red : (playerNumber == 2) ? Color.blue : (playerNumber == 3) ? Color.yellow : Color.grey;
+    }
+}
